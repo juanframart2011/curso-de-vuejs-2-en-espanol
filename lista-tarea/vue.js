7689ecc72edaebac46596e23d544e90ccebaea8c
juanframart2011 <juanframart2011@gmail.com>
2020-04-07 17:28:40 -0500

    section #5

--- lista-tarea/vue.js
+++ lista-tarea/vue.js
@@ -1,8 +1,4 @@
-var app = new Vue({
-
-	el: '#app',
-	data: {
-		titulo: 'Lista de Tareas',
+let data = {
 		tareas: [
 			{
 				texto: 'Aprende Vuej.Js',
@@ -18,9 +14,29 @@ var app = new Vue({
 			}
 		],
 		nuevaTarea: ''
+	};
+
+Vue.component( 'titulo',{
+	template: '<h2>{{ titulo }}</h2>',
+	data: function(){
+
+		return { titulo: 'Lista de Tareas' }
+	}
+})
+
+Vue.component( 'nueva-tarea',{
+	template: `<div class="input-group">
+					<input type="text" v-model="nuevaTarea" class="form-control" @keyup.enter="addTarea()">
+					<span class="input-group-btn">
+						<button @click="addTarea()" class="btn btn-primary mb-2">Agregar</button>
+					</span>
+				</div>`,
+	data: function(){
+
+		return data
 	},
 	methods:{
-		
+
 		addTarea: function(){
 
 			let texto = this.nuevaTarea.trim();
@@ -34,10 +50,37 @@ var app = new Vue({
 			}
 
 			this.nuevaTarea = '';
-		},
-		deleteTarea: function( tarea ){
-
-			this.tareas.splice( tarea, 1 );
 		}
 	}
+})
+
+Vue.component( 'lista-de-tareas',{
+	template: `<ul class="list-group">
+					    <li class="list-group-item" v-for="( tarea, i ) in tareas" :class="{ terminada: tarea.terminada }">
+					    	{{ tarea.texto }}
+					    	<span class="pull-right">
+
+					    		<button @click="tarea.terminada = !tarea.terminada" class="btn btn-success btn-xs glyphicon glyphicon-ok"></button>
+
+					    		<button @click="deleteTarea( i )" class="btn btn-danger btn-xs glyphicon glyphicon-ok"></button>
+					    	</span>
+					    </li>
+					</ul>`,
+	data: function(){
+
+		return data;
+	},
+	methods:{
+
+		deleteTarea: function( tarea ){
+
+			data.splice( tarea, 1 );
+		}
+	}
+})
+
+var app = new Vue({
+
+	el: '#app',
+	data: data
 });
