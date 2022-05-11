22700692d3cf109769d2840d3559564d6b6b0cda
juanframart2011 <juanframart2011@gmail.com>
2022-05-10 23:20:28 -0500

    section 9 formularios

---
+++ section-6/listatareas/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss"></style>
