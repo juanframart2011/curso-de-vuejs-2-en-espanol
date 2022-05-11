8de93e0ce3d50d2fade390c38e2c3d7dbe117c55
juanframart2011 <juanframart2011@gmail.com>
2022-05-10 23:42:26 -0500

    section 10 filters

---
+++ section-10-filters/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <div class="jumbotron">
+      <span>{{ mensaje | mayusculas }}</span>
+      <hr>
+      <span>{{ mensaje }}</span>
+      <hr>
+      <span>{{ mensaje | suspensivo }}</span>
+      <hr>
+      <span>{{ mensaje | suspensivo | mayusculas }}</span>
+      <hr>
+      <span>{{ temperatura }}</span>
+      <hr>
+      <span>{{ temperatura | aFahrenheit }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data(){
+    return{
+      mensaje: 'Aprende VueJS 2 free',
+      temperatura: 30
+    }
+  },
+  filters:{
+    mayusculas( mensaje ){
+      return mensaje.toUpperCase();
+    },
+    aFahrenheit( temperatura ){
+
+      return (9/5 * temperatura) + 32 + 'F';
+    }
+  }
+}
+</script>
+
+<style>
+</style>
